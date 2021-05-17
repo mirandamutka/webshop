877dfe3e5ecfba6c40792157e5e3e7c0ae9830b6
mirandamutka <michirisu@outlook.com>
2021-05-17 14:14:40 +0200

    Replaced mockdata with actual data from API

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,35 +1,86 @@
 import './MovieDetails.css';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-const MovieDetails = () => {
+const MovieDetails = (props) => {
+
+    const location = useLocation();
+
+    const [movieID, setMovieID] = useState();
+    const [movieName, setMovieName] = useState();
+    const [moviePoster, setMoviePoster] = useState();
+    const [movieRating, setMovieRating] = useState();
+    const [movieGenre, setMovieGenre] = useState();
+    const [moviePlot, setMoviePlot] = useState();
+    const [movieDirector, setMovieDirector] = useState();
+    const [movieActors, setMovieActors] = useState();
+    const [movieLanguage, setMovieLanguage] = useState();
+    const [movieCountry, setMovieCountry] = useState();
+    
+
+    useEffect(() => {
+        setMovieID(location.state.detail.imdbID);
+     }, [location]);
+
+    useEffect(() => {
+        getSelectedMovie();
+    }, [movieID]);
+
+    const getSelectedMovie = async () => {
+        if (movieID) {
+            const movieIDURL = `http://www.omdbapi.com/?i=${movieID}&apikey=b7ed0243`;
+            let response = await fetch(movieIDURL);
+            let data = await response.json();
+            if (data != null) {
+                setMovieName(data.Title);
+                setMoviePoster(data.Poster);
+                setMovieRating(data.Rated);
+                setMovieGenre(data.Genre);
+                setMoviePlot(data.Plot);
+                setMovieDirector(data.Director);
+                setMovieActors(data.Actors);
+                setMovieLanguage(data.Language);
+                setMovieCountry(data.Country);
+            }
+        }
+    }
+
     return (
         <div className="movieDetailsContainer">
-            <div className="closeWindow">✕</div>
-            <section className="imgBox">
-                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/1/11/MisfitsTeaserPoster.jpg/220px-MisfitsTeaserPoster.jpg" className="moviePoster" alt="" />
-            </section>
-            <section className="infoBox">
-                <section className="topInfo">
-                    <h3>The Misfits</h3>
-                    <section className="ratingGenre">
-                        <div className="movieRating">R</div> | 
-                        <div className="movieGenre">Action, Adventure, Thriller</div>
-                    </section>
-                    <div className="starRating">★★★★☆</div>
-                    <div className="moviePlot">
-                    After being recruited by a group of unconventional thieves, renowned criminal Richard Pace finds himself caught up in an elaborate gold heist that promises to have far-reaching implications on his life and the lives of countless others.
-                    </div>
-                    <div className="directorName">Director <b>Renny Harlin</b></div>
-                    <div className="actorsList">Actors <b>Jamie Chung, Pierce Brosnan, Tim Roth, Nick Cannon</b></div>
+            {movieName 
+            ? (
+            <>
+                <div className="closeWindow">✕</div>
+                <section className="imgBox">
+                    <img src={moviePoster} className="moviePoster" alt="" />
                 </section>
-                <section className="bottomInfo">
-                    <section className="languageCountry">
-                        <div className="movieLanguage">Language <b>English</b></div>
-                        <div className="movieCountry">Country <b>USA</b></div>
+                <section className="infoBox">
+                    <section className="topInfo">
+                        <h3>{movieName}</h3>
+                        <section className="ratingGenre">
+                            <div className="movieRating">{movieRating}</div> | 
+                            <div className="movieGenre">{movieGenre}</div>
+                        </section>
+                        <div className="starRating">★★★★☆</div>
+                        <div className="moviePlot">
+                            {moviePlot}
+                        </div>
+                        <div className="directorName">Director <b>{movieDirector}</b></div>
+                        <div className="actorsList">Actors <b>{movieActors}</b></div>
                     </section>
-                    {/* Skapa en separat knapp komponent som kan återanvändas */}
-                    <button className="buyButton">Buy 10$</button>
+                    <section className="bottomInfo">
+                        <section className="languageCountry">
+                            <div className="movieLanguage">Language <b>{movieLanguage}</b></div>
+                            <div className="movieCountry">Country <b>{movieCountry}</b></div>
+                        </section>
+                        {/* Skapa en separat knapp komponent som kan återanvändas */}
+                        <button className="buyButton">Buy 10$</button>
+                    </section>
                 </section>
-            </section>
+            </>
+            )
+            : <h3>Loading...</h3>
+            }
         </div>
     )
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const MovieList = (props) => {
 	const CartItem = props.cartItem;
-
+	let history = useHistory();
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div >
-					<img src={movie.Poster} alt='movie'></img>
+				<div key={index}>
+					<img src={movie.Poster} alt='movie' onClick={() => history.push({
+						pathname: '/MovieDetails',
+						state: { detail: movie }
+					})}></img>
 					<div onClick={() => props.handleBuyClick(movie)}>
 						<CartItem />
 					</div>
