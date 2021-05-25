0b4d8c0287915a51acd958e700588241dfe75733
sarakluge <Paddel2012>
2021-05-25 13:07:52 +0200

    added new release list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
-import ListItem from './ListItem';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddToCart from './components/AddToCart';
+import RemoveFromCart from './components/RemoveFromCart';
+import ListItem from './components/ListItem';
+import NewReleaseList from './components/NewReleaseList';
 
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [cart, setCart] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-//a comment from Sara Kluge
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b7ed0243`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      
-    </div>
-  );
-}
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieInCart = JSON.parse(
+			localStorage.getItem('react-movie-app-cart')
+		);
+
+		if (movieInCart) {
+			setCart(movieInCart);
+		}
+	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-cart', JSON.stringify(items));
+	};
+
+	const addToCart = (movie) => {
+		const newCart = [...cart, movie];
+		setCart(newCart);
+		saveToLocalStorage(newCart);
+	};
+
+	const removeFromCart = (movie) => {
+		const newCart = cart.filter(
+			(cart) => cart.imdbID !== movie.imdbID
+		);
+
+		setCart(newCart);
+		saveToLocalStorage(newCart);
+	};
+
+	return (
+		<div>
+			<div>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+      <NewReleaseList handleBuyClick={addToCart}/>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleBuyClick={addToCart}
+					cartItem={AddToCart}
+				/>
+			</div>
+			<div>
+				<MovieListHeading heading='Shopping Cart' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={cart}
+					handleBuyClick={removeFromCart}
+					cartItem={RemoveFromCart}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,11 +4,10 @@ import { MdShoppingCart } from "react-icons/md"
 
 
 const ListItem = () => {
-
-
+    
     return (
         <div className="list-item">
-            <img className="list-item-image" src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" alt="movie poster"></img>
+            <img className="list-item-image" src="" alt="movie poster"></img>
             <h3 className="title">Titel</h3>
             <p className="genre">(Genre)</p>
             <p className="director">By Renny Harlin</p>
